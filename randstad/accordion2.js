b5049877d86a335418b089dc03f91efd05991309
hireclix <steve.rouse@hireclix.com>
2016-12-07 09:17:01 -0500

    Update accordion2.js

--- randstad/accordion2.js
+++ randstad/accordion2.js
@@ -1,12 +1,19 @@
 $('.accordion').click(function(){
+    
 
   if( $(this).hasClass('active') ){
     $(this).removeClass('active');
     $(this).next().removeClass('show');
-  }else{
+  }
+    if($(this).hasClass('active')){
+        e.preventDefault();
+        e.stopPropagation();
+    }
+    
+    else{
     $('.accordion').removeClass('active');
     $('.panel').removeClass('show');
-
+      
     $(this).addClass('active');
     $(this).next().addClass('show');
   }
