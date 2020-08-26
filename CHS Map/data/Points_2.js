79ee7379a9dfb8aa87c70d6e12dccb30f9098590
superfonz <asanfilippo2014@gmail.com>
2020-08-26 16:40:15 -0400

    Add files via upload

---
+++ CHS Map/data/Points_2.js
@@ -0,0 +1,52 @@
+var json_Points_2 = {
+  "type": "FeatureCollection",
+  "name": "Points_2",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Hospital": "Northwest Medical Center",
+        "City": "Tucson",
+        "State": "AZ",
+        "POC": "Leaha Hicks, Anya Nelson, Tiffany Rivera",
+        "Status":"Media Plan Sent",
+        "Notes": "Had intro call and sent over media plan and market research. Also had follow up call to discuss direct response focused media plan. They were interested in what a branding media proposal would look like (call scheduled for 8/20 6 pm ET to walk through additional tactics) and will be discussing transferring of Indeed Sponsored campaigns to HireClix with updated priorities. Indeed Virtual Hiring Events demo scheduled for 8/18 with the team. Potentially scheduling demo with NurseFly pending Director feedback (Amy had separate call with the vendor)",
+        "Latitude": 32.2226066,
+        "Longitude": -110.9747108
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -110.9747108,
+            32.2226066
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Hospital": "Santa Rosa Medical Center",
+        "City": "Milton",
+        "State": "FL",
+        "POC": "Marilyn Carrasquillo",
+        "Status":"Campaigns Live",
+        "Notes": "Indeed campaigns live through sponsored feed. Working to set time to discuss recruiting for ICU RNs",
+        "Latitude": 30.6324149,
+        "Longitude": -87.0396881
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.0396881,
+            30.6324149
+        ]
+      }
+    }
+  ]
+}
